25d0562233bbe34dc79246261def5b93d2570c84
LeonardoFilesi <118814918+LeonardoFilesi@users.noreply.github.com>
2023-09-24 18:20:27 +0200

    Correzioni prof

--- net-ef-videogame/Videogame.cs
+++ net-ef-videogame/Videogame.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -14,18 +14,14 @@ namespace net_ef_videogame
         public string Name { get; set; }
         public string Overview { get; set; }
         public DateTime Release_date { get; set; }
-        public DateTime Created_at { get; set; }
-        public DateTime Updated_at { get; set;}
         public long Software_house_id { get; set; }
 
-        public Videogame(long id, string name, string overview, DateTime release_date, DateTime created_at, DateTime updated_at, long software_house_id)
+        public Videogame(long id, string name, string overview, DateTime release_date, long software_house_id)
         {
             Id = id;
             Name = name;
             Overview = overview;
             Release_date = release_date;
-            Created_at = created_at;
-            Updated_at = updated_at;
             Software_house_id = software_house_id;
         }
 
@@ -33,6 +29,6 @@ namespace net_ef_videogame
         {
             return $"ID: {Id}, {Name} -- {Overview} -- {Release_date}";
         }
-    }          
-   
+    }
+
 }
